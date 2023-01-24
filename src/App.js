41435cc8b04f093b2c7bbb7a1aaea6e6df2d6296
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-01-24 20:39:31 +0530

    Make Responce css

--- src/App.js
+++ src/App.js
@@ -99,11 +99,10 @@ function App() {
   }
   console.log(datas[0].meanings[0].definitions[0].definition);
 
-// const audio=new Audio(da)
   return (
     <div className="capitalize">
 
-      <nav className='flex justify-between items-center  mx-6 mt-4 text-white'>
+      <nav className=' lg:justify-around lg:mx-32 flex justify-between items-center  mx-6 mt-4 text-white'>
         <div className=''>
           <img width={"44px"} src={dic} alt="" />
         </div>
@@ -122,11 +121,11 @@ function App() {
           </div>
         </div>
       </nav>
-      <main className=" sm:mx-2 flex my-5 sm:justify-between flex-col ">
+      <main className=" lg:mx-80 lg:my-12 lg:justify-around sm:mx-2 flex my-5 sm:justify-between flex-col ">
         <div className="flex justify-center">
           <div className="xl:w-96">
             <div className="input-group relative flex  items-stretch w-full mb-4">
-              <input type="search" onChange={handlechange} className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
+              <input type="search" onChange={handlechange} className="form-control relative flex-auto min-w-0 block w-full lg:w-96 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
               <button onClick={() => { dataFetch(words) }} className="btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                   <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
@@ -145,7 +144,7 @@ function App() {
         </div>
         <div className='flex mx-6 my-4 justify-center items-center'>
           <h1 className='mr-1 font-bold'>Noun </h1>
-          <hr className='border-1 w-72' />
+          <hr className='border-1 w-72 lg:w-full' />
         </div>
         <div className=" mx-6">
           <label className="text-gray-400" htmlFor="meaning">Meaning</label>
@@ -157,7 +156,7 @@ function App() {
         </div>
         <div className='flex mx-6 my-4 justify-center items-center'>
           <h1 className='mr-1 font-bold'>Verb </h1>
-          <hr className='border-1 w-72' />
+          <hr className='border-1 w-72 lg:w-full' />
         </div>
         <div className='mx-6'>
           <label className="text-gray-400" htmlFor="meaning">Meaning</label>
